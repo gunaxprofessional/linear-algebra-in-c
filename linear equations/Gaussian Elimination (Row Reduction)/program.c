23b2524fa2a21bfa1c1755af6091cf05c8fbe044
Guna M <66107066+gunaxprofessional@users.noreply.github.com>
2024-10-26 22:47:33 +0530

    added user input

--- linear equations/Gaussian Elimination (Row Reduction)/program.c
+++ linear equations/Gaussian Elimination (Row Reduction)/program.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("just starting");
+    int unknowns;
+    printf("Enter the total unkowns ");
+    scanf("%d", &unknowns);
+
+    double **Coefficient = (double **)calloc(unknowns, sizeof(double *));
+    for (int i = 0; i < unknowns; i++)
+        Coefficient[i] = (double *)calloc(unknowns, sizeof(double));
+
+    for (int i = 0; i < unknowns; i++)
+    {
+        for (int j = 0; j < unknowns; j++)
+        {
+            printf("%dth %dth: ", i, j);
+            scanf("%lf", &Coefficient[i][j]);
+        }
+    }
+
+    for (int i = 0; i < unknowns; i++)
+    {
+        for (int j = 0; j < unknowns; j++)
+        {
+            printf("%.2lf ", Coefficient[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
